40f9c09a4a64144f84484edbc532f14edd442e1b
Uday Patel <uday3patel@gmail.com>
2026-04-17 12:59:10 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,37 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        memo = dict()
+
+        def dfs(index):
+            
+            if index in memo:
+                return memo[index]
+            
+            # 1. base case
+            if index >= len(nums):
+                return 0
+            
+            # 2. recursive case
+            '''
+            max money from robbing cur house and skipping the next (index + 2)
+            
+            OR
+
+            don't take the money from cur house and go to the next house -> dfs(index + 1)
+            '''
+            ways = max(
+                nums[index] + dfs(index + 2),
+                dfs(index + 1)
+            )
+
+            memo[index] = ways
+
+            return ways
+
+        return dfs(0)
+        
+        
+
+
+        
