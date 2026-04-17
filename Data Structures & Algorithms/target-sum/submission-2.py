00f0e3c55f9f7e9b83bdbc347d0a0d86057b9ebd
Uday Patel <uday3patel@gmail.com>
2026-04-17 11:50:40 -0400

    Add: target-sum - submission-2

---
+++ Data Structures & Algorithms/target-sum/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+
+        memo = dict()
+
+        def dfs(index, current_sum):
+
+            if (index, current_sum) in memo:
+                return memo[(index, current_sum)]
+
+            if index == len(nums):
+
+                if current_sum == target:
+                    return 1
+                else:
+                    return 0
+            
+            ways = dfs(index + 1, current_sum + nums[index]) + dfs(index + 1, current_sum - nums[index])
+
+            memo[(index, current_sum)] = ways
+
+            return ways
+
+        return dfs(0, 0)
+        
