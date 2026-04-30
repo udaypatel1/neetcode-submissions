ce58d8a28742c63b2d78abb4dfb36053fc058056
Uday Patel <uday3patel@gmail.com>
2026-04-30 14:35:54 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+
+        '''
+        permute all of s
+
+        for all permute of s, check if it maps to mapping
+
+        get total count of it
+        '''
+
+        n = len(s)
+        memo = {}
+
+        def dfs(i):
+
+            if i == n:
+                return 1  # valid split reached end
+
+            if s[i] == '0':
+                return 0  # invalid encoding
+
+            if i in memo:
+                return memo[i]
+
+            # take 1 digit
+            ways = dfs(i + 1)
+
+            # take 2 digits (if valid)
+            if i + 1 < n and int(s[i:i+2]) <= 26:
+                ways += dfs(i + 2)
+
+            memo[i] = ways
+            return ways
+
+        return dfs(0)
