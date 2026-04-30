2da2ef50896cad9ad61549a905e3a57e67b0f16f
Uday Patel <uday3patel@gmail.com>
2026-04-30 14:17:10 -0400

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,63 @@
+from collections import defaultdict
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        # a valid tree means there isnt a cycle
+        # at each iteration, check to see if the tree is valid
+        # by making sure a cycle doesnt exist
+
+        '''
+        given edges, we want to make sure
+        that no child node points to anything but its parent
+        a parent can have many children, but a child can only have 1 parent
+        multiple parents = cycle
+        '''
+
+        '''
+        what data structure would help us?
+
+        Note: this is a union find problem that I have not practiced yet
+        '''
+
+        # quick check
+
+        if len(edges) != n - 1:
+            return False
+
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        visited = set()
+
+        def dfs(node, parent):
+
+            visited.add(node)
+
+            for nei in graph[node]:
+                if nei == parent:
+                    continue # don't go back up the tree
+            
+                if nei in visited:
+                    return False # cycle detected, immediately
+                
+                if not dfs(nei, node):
+                    return False
+
+            return True
+        
+        # start DFS from node 0
+        if not dfs(0, -1):
+            return False
+
+        # must be fully connected
+        return len(visited) == n
+
+        
+
+
+
+        
