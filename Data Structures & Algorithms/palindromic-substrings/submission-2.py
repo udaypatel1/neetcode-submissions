f8f44fc3fc1c5390653d0ec2cf9474b5ddc9d28e
Uday Patel <uday3patel@gmail.com>
2026-04-30 13:40:15 -0400

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        n = len(s)
+        count = 0
+
+        # center expansion
+
+        def expand(left, right):
+            nonlocal count
+
+            # boundary check and if both chars are equal
+            while left >= 0 and right < n and s[left] == s[right]:
+
+                count += 1
+                left -= 1
+                right += 1
+
+        for i in range(n):
+
+            # odd length palindromes
+            expand(i, i)
+
+            # even length palindromes
+            expand(i, i+1)
+
+        return count
